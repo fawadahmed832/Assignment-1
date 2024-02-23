18fa755d3ff13e6634f4662fcdf595bf56c6cba8
Fawad Ahmed <53002637+fawadahmed832@users.noreply.github.com>
2024-02-24 03:59:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let personame = "Fawad";
+let age = "26";
+let message = `My Name is ${personame} and i am ${age} year old`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let personame:  string ="Fawad";
+let age: string= "26";
+let message: string= `My Name is ${personame} and i am ${age} year old`
+console.log (message);
